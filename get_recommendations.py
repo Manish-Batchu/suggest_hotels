77f84b5138aa48e083a938f32b5df42132919411
B-Manish <batchumanish@gmail.com>
2025-05-16 17:27:39 +0530

    Added similar_hotels

--- get_recommendations.py
+++ get_recommendations.py
@@ -26,3 +26,25 @@ def get_recommendations_for_email(email: str = Query(..., description="User's em
         }
     else:
         raise HTTPException(status_code=404, detail="No recommendations found for this email")
+    
+
+
+@app.get("/get_similar_hotels")
+def h2h(hotelname: str = Query(..., description="hotel's name")):
+    query = {"query": {
+        "term": {
+            "hotel_name.keyword": hotelname
+        }
+    }
+    }
+
+    response = es.search(index="h2h", body=query)
+
+    if response['hits']['total']['value'] > 0:
+        similar_hotels = response['hits']['hits'][0]['_source']['similar_hotels']
+        return {
+            "hotel_name": hotelname,
+            "similar_hotels": similar_hotels
+        }
+    else:
+        raise HTTPException(status_code=404, detail="No similar_hotels found for this hotel")    
